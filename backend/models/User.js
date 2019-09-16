110638933426cccac2b6d88e402399ab581c706a
Edmond <e.pan2@hotmail.com>
2019-09-16 23:46:48 +1000

    Added routes for the user plus password hashing in User Model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,8 @@
 // This is the data model for a user of our application
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcryptjs");
 
 
 const UserSchema = new Schema(
@@ -12,7 +13,11 @@ const UserSchema = new Schema(
             type: String,
             isRequired: true
         },
-        "publicName": String,
+        // Change this later, just for testing login
+        "name": {
+            type: String,
+            isRequired: true
+        },
         "firstName": String,
         "lastName": String,
         "DOB": Date,
@@ -27,6 +32,28 @@ const UserSchema = new Schema(
         }
     }
 );
+
+// Now hash the password before we completely save the new user into the database
+UserSchema.pre('save', function(next) {
+    var user = this;
+    var SALT_FACTOR = 10;
+
+    if (!user.isModified('password')) {
+        return next();
+    }
+    bcrypt.genSalt(SALT_FACTOR, function(err, salt) {
+        if (err) {
+            return err;
+        }
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) {
+                return err;
+            }
+            user.password = hash;
+            next();
+        });
+    });
+});
 
 // Export as a mongoose model which will be stored in a collection called users
 module.exports =  User = mongoose.model("users", UserSchema);
