61fcdc7e5d13187bd2de63ccea25ee988afb0bec
Judith <jchhoeur@student.unimelb.edu.au>
2019-09-24 16:02:20 +1000

    Updated profile page and corresponding components
    This profile page is intended to be viewed by other users so very basic information is shown. Duplicated nav bar and made one specifically for profile page as the links will differ from the welcome page. Still figuring out how to make it look nice

---
+++ frontend/src/components/layout/profileNavBar.js
@@ -0,0 +1,67 @@
+import React, {Component} from 'react';
+import {
+    Button,
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Nav,
+    NavItem,
+    NavLink,
+    Container,
+    Row,
+    Col,
+    DropdownMenu,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownItem,
+} from 'reactstrap';
+
+const LOGO = '/images/storyQuest.png'
+
+class TopMenu extends Component {
+
+    render() {
+        return(
+            <header>
+                <Navbar fixed="top" color="white" light expand="xs" className="border-bottom border-gray" style={{height: 70}}>
+                    {/* Use grid system to create 1 row and 3 columns to put each of the Navbar elements into */}
+                    <NavbarToggler onClick={this.toggle}/>
+                    <Container>
+                        <Row noGutters className="position-relative w-100 align-items-center">
+                            <Col className="d-none d-lg-flex justify-content-start">
+                                <Nav className="mlx-auto" navbar>
+                                    <NavItem className="d-flex align-items-center">
+                                        <NavLink className="font-weight-bold " href="/">Home</NavLink>
+                                    </NavItem>
+                                    <NavItem className="d-flex align-items-center">
+                                        <NavLink className="font-weight-bold " href="/">My Artifacts</NavLink>
+                                    </NavItem>
+                                </Nav>
+                            </Col>
+
+                            <Col className="d-flex justify-content-xs-center justify-content-lg-center">
+                                <NavbarBrand className="d-inline-block p-1" href="/" style={{ width: 80 }}>
+                                    <img src={LOGO} alt="logo" className="position-relative img-fluid rounded-sm" />
+                                </NavbarBrand>
+                            </Col>
+
+                            <Col className="d-none d-lg-flex justify-content-end">
+                                <Nav className="mrx-auto" navbar>
+                                    <UncontrolledDropdown className="d-flex align-items-center" nav inNavbar>
+                                        <DropdownToggle className="font-weight-bold" nav caret><i className="fa fa-user-circle" style={{fontSize: "36px"}} /></DropdownToggle>
+                                        <DropdownMenu right>
+                                            <DropdownItem href="/profile"><Button>My Profile</Button></DropdownItem>
+                                            <DropdownItem href="/logout"><Button>Logout</Button></DropdownItem>
+                                        </DropdownMenu>
+                                    </UncontrolledDropdown>
+                                </Nav>
+                            </Col>
+                        </Row>
+                    </Container>
+                </Navbar>
+            </header>
+        )
+    };
+}
+
+export default TopMenu;
